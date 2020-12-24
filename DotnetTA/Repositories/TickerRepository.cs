37b47001c81f63be3946501f415dad084e71110e
YoureaBosonHarry <rath3.14159@protonmail.com>
2020-12-23 22:28:38 -0600

    Add project files.

---
+++ DotnetTA/Repositories/TickerRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetTA.Repositories
+{
+    public class TickerRepository
+    {
+        private readonly string connectionString;
+
+        public TickerRepository(string connectionString) {
+            this.connectionString = connectionString;
+        }
+
+        public async Task
+    }
+}
